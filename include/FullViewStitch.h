98e80658f3d420a22a563d9d11436affcea86ff5
= <=>
2016-06-25 10:27:07 +0800

    h file

---
+++ include/FullViewStitch.h
@@ -0,0 +1,34 @@
+#ifndef __FULL__VIEW_STITCH__
+#define __FULL__VIEW_STITCH__
+
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "opencv2/opencv_modules.hpp"
+#include <opencv2/core/utility.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/stitching/detail/autocalib.hpp"
+#include "opencv2/stitching/detail/blenders.hpp"
+#include "opencv2/stitching/detail/timelapsers.hpp"
+#include "opencv2/stitching/detail/camera.hpp"
+#include "opencv2/stitching/detail/exposure_compensate.hpp"
+#include "opencv2/stitching/detail/matchers.hpp"
+#include "opencv2/stitching/detail/motion_estimators.hpp"
+#include "opencv2/stitching/detail/seam_finders.hpp"
+#include "opencv2/stitching/detail/util.hpp"
+#include "opencv2/stitching/detail/warpers.hpp"
+#include "opencv2/stitching/warpers.hpp"
+
+
+
+using namespace std;
+using namespace cv;
+using namespace cv::detail;
+
+
+
+#endif
+
---
+++ include/kklog.h
@@ -0,0 +1,8 @@
+#ifndef __KKLOG__H
+#define __KKLOG__H
+
+//#define LOGK(x, ...) printf("%s(%d), "x"\n", __FUNCTION__, __LINE__, ##__VA_ARGS__);
+//#define LOGLN(x) {printf("%s(%d):\n ",__FUNCTION__,__LINE__); cout<<x<<endl;}
+
+#endif
+
